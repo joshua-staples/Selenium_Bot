4037a548ea60981bde2daec1a83c35d0306c80ee
Josh S <josh@stapleszoo.com>
2021-12-01 12:36:39 -0700

    finished as of Dec 1, 2021

--- amazon_bot.py
+++ amazon_bot.py
@@ -4,41 +4,94 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 class amazon_bot():
+
     def __init__(self, usernm, pswd, prod_url):
         self.usernm = usernm
         self.pswd = pswd
         self.prod_url = prod_url
         self.driver = webdriver.Safari()
 
-    # def login_amazon():
-#     driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
-#     # driver.get('https://www.amazon.com/AmazonBasics-Performance-Alkaline-Batteries-8-Pack/dp/B00O869QUC?ref_=ast_sto_dp&th=1&psc=1')
-
-#     # input email into the field and submit
-#     with open('email.txt') as f:
-#         email = f.read()
-
-#     driver.find_element(By.XPATH, '//*[@id="ap_email"]').send_keys(email, Keys.RETURN)
-#     time.sleep(5)
-
-#     # input password into field and submit
-#     with open('password.txt') as f:
-#         password = f.read()
-    
-#     driver.find_element(By.XPATH, '//*[@id="ap_password"]').send_keys(password, Keys.RETURN)
+    def login_amazon(self):
+        """Logs into your Amazon account.
+        """
+        #login to Amazon account with email/passwrd
+        # print(f'Login: {self.driver.session_id}')
+        self.driver.find_element(By.XPATH, '//*[@id="ap_email"]').send_keys(self.usernm, Keys.RETURN)
+        time.sleep(4)
+        self.driver.find_element(By.XPATH, '//*[@id="ap_password"]').send_keys(self.pswd, Keys.RETURN)
+        # had to increase wait time because it asks for approval now....they're on to me
+        time.sleep(10)
 
     def add_product_to_cart(self):
-    # link to AAA batteries
+        """Adds a product to your cart based on the url you input. Clicks the add to cart and checkout buttons.
+        """
+        # add some error handling for different product links
+        # print(f'Add product to cart: {self.driver.session_id}')
         self.driver.get(self.prod_url)
-        time.sleep(5)
-        self.driver.find_element(By.XPATH, '//*[@id="add-to-cart-button"]').click()    
-        time.sleep(5)
-        self.driver.find_element(By.XPATH, '//*[@id="hlb-ptc-btn-native"]').click()
-        time.sleep(5)
-        self.driver.close()
-    
+        try:
+            if self.driver.find_element(By.XPATH, '//*[@id="newAccordionCaption_feature_div"]/div').is_enabled():
+                self.driver.find_element(By.XPATH, '//*[@id="newAccordionRow"]/div/div[1]/a').click()
+                time.sleep(6)
+                self.driver.find_element(By.XPATH, '//*[@id="add-to-cart-button"]').click()    
+                time.sleep(4)
+                self.driver.find_element(By.XPATH, '//*[@id="hlb-ptc-btn-native"]').click()
+                time.sleep(4)
+            else:
+                self.driver.find_element(By.XPATH, '//*[@id="add-to-cart-button"]').click()    
+                time.sleep(4)
+                self.driver.find_element(By.XPATH, '//*[@id="hlb-ptc-btn-native"]').click()
+                time.sleep(4)
+        except:
+            self.driver.find_element(By.XPATH, '//*[@id="add-to-cart-button"]').click()    
+            time.sleep(4)
+            self.driver.find_element(By.XPATH, '//*[@id="hlb-ptc-btn-native"]').click()
+            time.sleep(4)
+
+    # if you want to actually buy something then replace the print statement with .click() on the same 
+    # XPATH as the conditional statement.
     def checkout(self):
-        pass
+        """Checks to see if the place order button can be clicked (if the bot was successful)
+        """
+        # print(f'Checkout: {self.driver.session_id}')
+        if self.driver.find_element(By.XPATH, '//*[@id="placeYourOrder"]/span/input').is_enabled():
+            print("Order was able to be purchased....but was not purchased in order to save your wallet!")
 
-    
+    def shipping(self):
+        """Selects the default shipping methood from the list on the amazon checkout page.
+        """
+        # click the button to change shipping address, click the default address
+        # print(f'Shipping: {self.driver.session_id}')
+        self.driver.find_element(By.XPATH, '//*[@id="spc-top"]/div/div[1]/div[1]/div[1]/span/a').click()
+        time.sleep(4)
+        self.driver.find_element(By.XPATH, '//*[@id="shipping-address-popover"]/div[1]/div[2]/div/div[2]/div[3]/span[1]/span/a').click()
+        time.sleep(4)
 
+    def payment(self):
+        """Selects the default payment methood from the list of payments on the amazon checkout page.
+        """
+        # print(f'Payment: {self.driver.session_id}')
+        # click the change payment button
+        self.driver.find_element(By.XPATH, '//*[@id="payment-change-link"]').click()
+        time.sleep(4)
+
+        # check the default card payment option, if it already selected then press continue, this will remove the use of gift cards
+        if not self.driver.find_element(By.XPATH, '//*[@id="pp-pJ7Tjx-69"]').is_selected():
+            self.driver.find_element(By.XPATH, '//*[@id="pp-pJ7Tjx-69"]').click()
+        else:
+            self.driver.find_element(By.XPATH, '//*[@id="pp-pJ7Tjx-91"]/span/input').click()
+            time.sleep(4)
+
+    def close_session(self):
+        """Quits the current webdriver session
+        """
+        # print(f'Close session: {self.driver.session_id}')
+        time.sleep(5)
+        self.driver.quit()
+        
+    def get_url(self):
+        """This function is a getter function to return the current url of the webdriver
+
+        Returns:
+            String: current webdriver url
+        """
+        return self.driver.current_url
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from amazon_bot import amazon_bot as ab
+import smtplib
 
 def main():
     with open('password.txt') as f:
@@ -11,5 +12,46 @@ def main():
     url = input("Enter a Amazon Product Url: ")
     bot = ab(email, password, url)
     bot.add_product_to_cart()
+    bot.login_amazon()
+    bot.shipping()
+    # can't get payment switching working yet
+    # bot.payment()
+    bot.checkout()
+
+    # send an email when it adds to cart
+    # not working yet, but close...
+
+    # with open('email_password.txt') as f:
+    #     email_pass = f.read()
+
+    # gmail_user = email
+    # gmail_password = email_pass
+
+    # sent_from = gmail_user
+    # to = email
+    # subject = 'Item Added to Your Amazon Cart'
+    # body = f'Item added to your cart! Go to this link now to checkout! ({bot.get_url()})'
+
+    # email_text = """\
+    # From: %s
+    # To: %s
+    # Subject: %s
+
+    # %s
+    # """ % (sent_from, to, subject, body)
+
+    # try:
+    #     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    #     server.ehlo()
+    #     server.login(gmail_user, gmail_password)
+    #     server.sendmail(sent_from, to, email_text)
+    #     server.close()
+
+    #     print('Email sent!')
+    # except:
+    #     print('Something went wrong...')
+
+    # finally close the webdriver session
+    bot.close_session()
 
 main()
